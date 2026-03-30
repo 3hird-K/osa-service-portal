df5f530e21c038bf48eeb1f63612758efa2cc9fe
Neil Dime <dime.neil03@gmail.com>
2026-03-30 09:01:37 +0800

    Modified: QR-MObile for Homepage default

--- components/mobile-app-link.tsx
+++ components/mobile-app-link.tsx
@@ -12,7 +12,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { StatusIndicator } from "@/components/status-indicator";
 import { FeatureCard } from "@/components/feature-card";
-import MobileAppQR from "@/assets/mobile-app.png";
+import MobileAppQR from "@/assets/march-30-update.png";
 
 const mobileFeatures = [
   {
